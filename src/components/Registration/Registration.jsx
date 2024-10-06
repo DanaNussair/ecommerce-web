b02812fe664bc7f2998173c327ccd1affd9ea17d
Dana Alnusair <dana.nusair88@gmail.com>
2024-10-06 14:10:09 +0300

    Cleans up registration file and adds react-hook-form

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,64 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '../Shared/Button';
 import FormField from '../Shared/FormFields/FormField';
-
-let FORM_OPTIONS = [
-	{
-		id: 1,
-		name: 'Name',
-		fieldType: 'TEXT',
-		minLength: 1,
-		maxLength: 100,
-		defaultValue: 'John Doe',
-		required: true,
-	},
-	{
-		id: 2,
-		name: 'Age',
-		fieldType: 'NUMBER',
-		minLength: 1,
-		maxLength: 50,
-		defaultValue: '24',
-		required: true,
-	},
-	{
-		id: 3,
-		name: 'Email',
-		fieldType: 'EMAIL',
-		minLength: 1,
-		maxLength: 50,
-		defaultValue: 'hello@mail.com',
-		required: true,
-	},
-	{
-		id: 4,
-		name: 'Password',
-		fieldType: 'PASSWORD',
-		minLength: 1,
-		maxLength: 50,
-		defaultValue: 'hello@mail.com',
-		required: true,
-	},
-	{
-		id: 5,
-		name: 'Role',
-		fieldType: 'LIST',
-		defaultValue: 'customer',
-		required: true,
-		listOfValues: [
-			{ value: 'retailer', label: 'Retailer' },
-			{ value: 'customer', label: 'Customer' },
-		],
-	},
-	{
-		id: 6,
-		name: 'Love React?',
-		fieldType: 'RADIO',
-		defaultValue: '1',
-		required: true,
-		listOfValues: ['Yes', 'No'],
-	},
-];
+import { FORM_OPTIONS } from './constants';
 
 function Registration() {
 	const navigate = useNavigate();
